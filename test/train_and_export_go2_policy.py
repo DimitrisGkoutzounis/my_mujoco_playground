2103a57997acb8c5441a3bc3005a1b1c8bc9d4b4
despinar <despina.argi@gmail.com>
2025-03-25 13:20:18 +0200

    Resolve merge conflicts-add locomotions params and train

--- test/train_and_export_go2_policy.py
+++ test/train_and_export_go2_policy.py
@@ -11,10 +11,10 @@ import jax
 import tf2onnx
 import tensorflow as tf
 
-from keras import layers
+# from keras import layers
 
 import tensorflow as tf
-import keras
+# import keras
 import onnxruntime as rt
 from brax.training.acme import running_statistics
 from mujoco_playground.config import locomotion_params
@@ -35,6 +35,8 @@ from orbax import checkpoint as ocp
 
 import numpy as np
 import tensorflow as tf
+
+from tensorflow import keras
 
 
 
@@ -71,7 +73,7 @@ class MLP(tf.keras.Model):
 
         self.mlp_block = tf.keras.Sequential(name="MLP_0")
         for i, size in enumerate(self.layer_sizes):
-            dense_layer = layers.Dense(
+            dense_layer = keras.layers.Dense( #here
                 size,
                 activation=self.activation,
                 kernel_initializer=self.kernel_init,
@@ -80,7 +82,7 @@ class MLP(tf.keras.Model):
             )
             self.mlp_block.add(dense_layer)
             if self.layer_norm:
-                self.mlp_block.add(layers.LayerNormalization(name=f"layer_norm_{i}"))
+                self.mlp_block.add(keras.layers.LayerNormalization(name=f"layer_norm_{i}")) #here
         if not self.activate_final and self.mlp_block.layers:
             if hasattr(self.mlp_block.layers[-1], 'activation') and self.mlp_block.layers[-1].activation is not None:
                 self.mlp_block.layers[-1].activation = None
@@ -161,6 +163,13 @@ def transfer_weights(jax_params, tf_model):
 
 env_name = "Go2JoystickFlatTerrain"
 env_cfg = locomotion.get_default_config(env_name)
+# Change gait phase
+# env_cfg.command_config.a = [1.5, 0.8, 2 * jp.pi]  # Adjust for trot
+# env_cfg.pert_config.enable = True
+# env_cfg.pert_config.velocity_kick = [3.0, 6.0]
+# env_cfg.pert_config.kick_wait_times = [5.0, 15.0]
+
+
 env = locomotion.load(env_name, config=env_cfg)
 
 
@@ -234,7 +243,7 @@ train_fn = functools.partial(
     randomization_fn=randomizer,
     progress_fn=progress
 )
-
+eval_env=registry.load(env_name, config=env_cfg)
 make_inference_fn, params, metrics = train_fn(
     environment=env,
     eval_env=registry.load(env_name, config=env_cfg),
@@ -294,3 +303,140 @@ providers = ['CPUExecutionProvider']
 m = rt.InferenceSession(output_path, providers=providers)
 
 print(f"✅ ONNX policy saved to: {output_path}")
+
+
+# Enable perturbation in the eval env.
+env_cfg = registry.get_default_config(env_name)
+env_cfg.pert_config.enable = True
+env_cfg.pert_config.velocity_kick = [3.0, 6.0]
+env_cfg.pert_config.kick_wait_times = [5.0, 15.0]
+env_cfg.command_config.a = [1.5, 0.8, 2*jp.pi]
+# env_cfg.gait_config.phase_offsets = [0.0, 0.5]  # Front-left/back-right in sync, front-right/back-left in sync
+
+eval_env = registry.load(env_name, config=env_cfg)
+velocity_kick_range = [0.0, 0.0]  # Disable velocity kick.
+kick_duration_range = [0.05, 0.2]
+
+jit_reset = jax.jit(eval_env.reset)
+jit_step = jax.jit(eval_env.step)
+jit_inference_fn = jax.jit(make_inference_fn(params, deterministic=True))
+
+
+# The following code collects trajectory data
+# @title Rollout and Render
+from mujoco_playground._src.gait import draw_joystick_command
+
+x_vel = 1.0  #@param {type: "number"}
+y_vel = 0.0  #@param {type: "number"}
+# yaw_vel = 3.14  #@param {type: "number"}
+yaw_vel = 0.4  #@param {type: "number"}
+
+def sample_pert(rng):
+  rng, key1, key2 = jax.random.split(rng, 3)
+  pert_mag = jax.random.uniform(
+      key1, minval=velocity_kick_range[0], maxval=velocity_kick_range[1]
+  )
+  duration_seconds = jax.random.uniform(
+      key2, minval=kick_duration_range[0], maxval=kick_duration_range[1]
+  )
+  duration_steps = jp.round(duration_seconds / eval_env.dt).astype(jp.int32)
+  state.info["pert_mag"] = pert_mag
+  state.info["pert_duration"] = duration_steps
+  state.info["pert_duration_seconds"] = duration_seconds
+  return rng
+
+rng = jax.random.PRNGKey(0)
+rollout = []
+modify_scene_fns = []
+
+swing_peak = []
+rewards = []
+linvel = []
+angvel = []
+track = []
+foot_vel = []
+rews = []
+contact = []
+command = jp.array([x_vel, y_vel, yaw_vel])
+
+state = jit_reset(rng)
+if state.info["steps_since_last_pert"] < state.info["steps_until_next_pert"]:
+  rng = sample_pert(rng)
+state.info["command"] = command
+for i in range(env_cfg.episode_length):
+  if state.info["steps_since_last_pert"] < state.info["steps_until_next_pert"]:
+    rng = sample_pert(rng)
+  act_rng, rng = jax.random.split(rng)
+  ctrl, _ = jit_inference_fn(state.obs, act_rng)
+  state = jit_step(state, ctrl)
+  state.info["command"] = command
+  rews.append(
+      {k: v for k, v in state.metrics.items() if k.startswith("reward/")}
+  )
+  rollout.append(state)
+  swing_peak.append(state.info["swing_peak"])
+  rewards.append(
+      {k[7:]: v for k, v in state.metrics.items() if k.startswith("reward/")}
+  )
+  linvel.append(env.get_global_linvel(state.data))
+  angvel.append(env.get_gyro(state.data))
+  track.append(
+      env._reward_tracking_lin_vel(
+          state.info["command"], env.get_local_linvel(state.data)
+      )
+  )
+
+  feet_vel = state.data.sensordata[env._foot_linvel_sensor_adr]
+  vel_xy = feet_vel[..., :2]
+  vel_norm = jp.sqrt(jp.linalg.norm(vel_xy, axis=-1))
+  foot_vel.append(vel_norm)
+
+  contact.append(state.info["last_contact"])
+
+  xyz = np.array(state.data.xpos[env._torso_body_id])
+  xyz += np.array([0, 0, 0.2])
+  x_axis = state.data.xmat[env._torso_body_id, 0]
+  yaw = -np.arctan2(x_axis[1], x_axis[0])
+  modify_scene_fns.append(
+      functools.partial(
+          draw_joystick_command,
+          cmd=state.info["command"],
+          xyz=xyz,
+          theta=yaw,
+          scl=abs(state.info["command"][0])
+          / env_cfg.command_config.a[0],
+      )
+  )
+
+
+# The video is being saved
+render_every = 2
+fps = 1.0 / eval_env.dt / render_every
+traj = rollout[::render_every]
+mod_fns = modify_scene_fns[::render_every]
+
+scene_option = mujoco.MjvOption()
+scene_option.geomgroup[2] = True
+scene_option.geomgroup[3] = False
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = False
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_PERTFORCE] = True
+
+frames = eval_env.render(
+    traj,
+    camera="track",
+    scene_option=scene_option,
+    width=640,
+    height=480,
+    modify_scene_fns=mod_fns,
+)
+video_output_path = "./vid_go2/"
+os.makedirs(video_output_path, exist_ok=True)  # Create the directory if it doesn't exist
+
+# Set video filename
+video_filename = os.path.join(video_output_path, "simulation_output.mp4")
+
+# Save the video locally instead of displaying it
+media.write_video(video_filename, frames, fps=fps)
+
+print(f"Video saved at: {video_filename}")
