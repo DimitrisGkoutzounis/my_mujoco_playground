e02cf7d1ba20f6ce93552c60c7a21aec396837ab
despinar <despina.argi@gmail.com>
2025-03-25 15:04:32 +0200

    Import from .py

--- mujoco_playground/experimental/sim2sim/Despinar_go2/play_go2_random_vel.py
+++ mujoco_playground/experimental/sim2sim/Despinar_go2/play_go2_random_vel.py
@@ -26,45 +26,13 @@ import onnxruntime as rt
 from mujoco_playground._src.locomotion.go2 import go2_constants
 from mujoco_playground._src.locomotion.go2.base import get_assets
 
+from Locomotion_Controller import Locomotion_Controller
+from Navigator import Navigator
+
 _HERE = epath.Path(__file__).parent
 _ONNX_DIR = _HERE.parent / "onnx" # Modified this (_HERE.parent), one folder back before access .onnx
 
-# Navigator Class: Decides nav. cmds - Generates random navigation commands
-class Navigator:
-    def __init__(self, vel_scale_x=1.5, vel_scale_y=0.8, vel_scale_rot=2*np.pi):
-        self.vel_scale_x=vel_scale_x
-        self.vel_scale_y=vel_scale_y
-        self.vel_scale_rot=vel_scale_rot
-        self.command = np.zeros(3)
 
-    # Random x,y,rotation
-    def generate_command(self):
-        # Random vel
-        self.vel_scale_x = np.around(np.random.uniform(-1, 1),3) # Random between 0-1, 3 dec.
-        self.vel_scale_y = np.around(np.random.uniform(-1, 1),3) # Random between 0-1, 3 dec.
-        self.vel_scale_rot = np.around(np.random.uniform(-1, 1)*2*np.pi,3) # Random between 0-2pi, 3 dec
-        # Pass it to command
-        self.command = np.zeros(3)
-        self.command[0] = self.vel_scale_x
-        self.command[1] = self.vel_scale_y
-        self.command[2] = self.vel_scale_rot
-
-        return self.command
-    
-    # Random x,y: no rotation
-    def generate_command_norot(self):
-        # Random vel
-        self.vel_scale_x = np.around(np.random.uniform(-1, 1),3) # Random between 0-1, 3 dec.
-        self.vel_scale_y = np.around(np.random.uniform(-1, 1),3) # Random between 0-1, 3 dec.
-        self.vel_scale_rot = 0.0
-        # Pass it to command
-        self.command = np.zeros(3)
-        self.command[0] = self.vel_scale_x
-        self.command[1] = self.vel_scale_y
-        self.command[2] = self.vel_scale_rot
-
-        return self.command
-    
 # NavigationPolicy Class: Defines the navigation policy, TBD
 #  Until now: it has Navigator() which decides randomly the vel cmds
 class NavigationPolicy:
@@ -99,80 +67,6 @@ class NavigationPolicy:
 
 
 
-class Locomotion_OnnxController:
-  """Low level - Locomotion ONNX controller for Go2 robot."""
-
-  def __init__(
-      self,
-      locomotion_policy_path: str,
-      default_angles: np.ndarray,
-      n_substeps: int,
-      action_scale: float = 0.5,
-      vel_scale_x: float = 1.5,
-      vel_scale_y: float = 0.8,
-      vel_scale_rot: float = 2 * np.pi,
-  ):
-    self._output_names = ["continuous_actions"]
-    self._policy = rt.InferenceSession(
-        locomotion_policy_path, providers=["CPUExecutionProvider"]
-    )
-
-    self._action_scale = action_scale
-    self._default_angles = default_angles
-    self._last_action = np.zeros_like(default_angles, dtype=np.float32)
-
-    self._counter = 0
-    self._n_substeps = n_substeps
-
-    self._joystick = Navigator(
-        vel_scale_x=vel_scale_x,
-        vel_scale_y=vel_scale_y,
-        vel_scale_rot=vel_scale_rot,
-    )
-
-  def get_obs(self, model, data) -> np.ndarray:
-    #print all detected sensors
-
-        
-    linvel = data.sensor("local_linvel").data
-    gyro = data.sensor("gyro").data
-    imu_xmat = data.site_xmat[model.site("imu").id].reshape(3, 3)
-    gravity = imu_xmat.T @ np.array([0, 0, -1])
-    joint_angles = data.qpos[7:] - self._default_angles
-    joint_velocities = data.qvel[6:]
-    obs = np.hstack([
-        linvel,
-        gyro,
-        gravity,
-        joint_angles,
-        joint_velocities,
-        self._last_action,
-        self._joystick.generate_command_norot(),
-    ])
-    return obs.astype(np.float32)
-
-  def get_control(self, model: mujoco.MjModel, data: mujoco.MjData) -> None:
-    self._counter += 1
-    if self._counter % self._n_substeps == 0:
-      obs = self.get_obs(model, data)
-      # print current time
-      onnx_input = {"obs": obs.reshape(1, -1)}
-      onnx_pred = self._policy.run(self._output_names, onnx_input)[0][0]
-      self._last_action = onnx_pred.copy()
-      data.ctrl[:] = onnx_pred * self._action_scale + self._default_angles
-
-  def custom_obs(self, model, data) -> np.ndarray:
-     
-        print(data.sensor("local_linvel").data)
-      
-  def custom_control(self, model: mujoco.MjModel, data:mujoco.MjData) -> None:
-     self._counter += 1
-
-     if self._counter % self._n_substeps == 0:
-
-        print(f"Simulation time: {data.time:.4f}")
-       
-
 
 def load_callback(model=None, data=None):
   # Set no mujoco control - default callback
@@ -191,7 +85,7 @@ def load_callback(model=None, data=None):
   n_substeps = int(round(ctrl_dt / sim_dt))
   model.opt.timestep = sim_dt
 
-  locomotion_policy = Locomotion_OnnxController(
+  locomotion_policy = Locomotion_Controller(
       locomotion_policy_path=(_ONNX_DIR / "go2_policy_galloping.onnx").as_posix(),
       default_angles=np.array(model.keyframe("home").qpos[7:]),
       n_substeps=n_substeps,
