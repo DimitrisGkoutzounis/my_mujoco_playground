eecbbe18466689308a89b28eeb97dc4cca9b4692
despinar <despina.argi@gmail.com>
2025-03-26 11:35:36 +0200

    Play with arm-loader

--- mujoco_playground/experimental/sim2sim/Despinar_go2/src/play_arm_go2_random_vel.py
+++ mujoco_playground/experimental/sim2sim/Despinar_go2/src/play_arm_go2_random_vel.py
@@ -28,11 +28,12 @@ import mujoco.viewer as viewer
 import numpy as np
 import onnxruntime as rt
 
-from mujoco_playground._src.locomotion.go2 import go2_constants
-from mujoco_playground._src.locomotion.go2.base import get_assets
+# from mujoco_playground._src.locomotion.go2 import go2_constants
+# from mujoco_playground._src.locomotion.go2.base import get_assets
 
 # This is mine! Place holder to add arm+go2
-# from mujoco_playground._src.dynamic_events import new_go2_constants
+from mujoco_playground._src.dynamic_events import new_go2_constants
+from mujoco_playground._src.dynamic_events.base  import get_assets
 
 from Locomotion_Controller import Locomotion_Controller
 from Navigator import Navigator
@@ -48,7 +49,7 @@ def main_function(model=None, data=None):
     mujoco.set_mjcb_control(None)
     # Load 
     model = mujoco.MjModel.from_xml_path(
-        go2_constants.FEET_ONLY_FLAT_TERRAIN_XML.as_posix(),
+        new_go2_constants.UR5E_GO2_SCENE.as_posix(),
         assets=get_assets(),
     )
     # Mujoco data
@@ -57,38 +58,38 @@ def main_function(model=None, data=None):
     mujoco.mj_resetDataKeyframe(model, data, 0)
     
 
-    # Define params
-    ctrl_dt = 0.02
-    sim_dt = 0.004
-    n_substeps = int(round(ctrl_dt / sim_dt))
-    model.opt.timestep = sim_dt
+    # # Define params
+    # ctrl_dt = 0.02
+    # sim_dt = 0.004
+    # n_substeps = int(round(ctrl_dt / sim_dt))
+    # model.opt.timestep = sim_dt
 
 
-    # Top Level Controller - Controls EVERYTHING
-    top_controller = TopLevelController()
-    # Locomotion Controller from Trained Onnx Policy
-    top_controller.locomotion_ctrl_ = Locomotion_Controller(
-        locomotion_policy_path=(_ONNX_DIR / "go2_policy_galloping.onnx").as_posix(),
-        default_angles=np.array(model.keyframe("home").qpos[7:]),
-        n_substeps=n_substeps,
-        action_scale=0.5,
-        vel_scale_x=1.5,
-        vel_scale_y=0.8,
-        vel_scale_rot=2 * np.pi,
-        locomotion_cmd=np.zeros(3) # Define which Navigator-Boss the locomotion cotroller will have.
-    )
-    # TBD: Future definition of Navigation Policy
-    top_controller.navigation_policy_ = NavigationPolicy(
-            default_angles=np.array(model.keyframe("home").qpos[7:]), #TBD: FIX THOSE DEFAULT ANGELS
-            n_substeps=n_substeps,  #TBD: FIX THOSE DEFAULT N_SUBSTEPS
-            action_scale=0.5,
-        )
-    # Init time variables
-    top_controller.t_last_cmd = data.time
-    top_controller.dt_new_cmd = 0.5 #0.5sec - update vel command per delta t
+    # # Top Level Controller - Controls EVERYTHING
+    # top_controller = TopLevelController()
+    # # Locomotion Controller from Trained Onnx Policy
+    # top_controller.locomotion_ctrl_ = Locomotion_Controller(
+    #     locomotion_policy_path=(_ONNX_DIR / "go2_policy_galloping.onnx").as_posix(),
+    #     default_angles=np.array(model.keyframe("home").qpos[7:]),
+    #     n_substeps=n_substeps,
+    #     action_scale=0.5,
+    #     vel_scale_x=1.5,
+    #     vel_scale_y=0.8,
+    #     vel_scale_rot=2 * np.pi,
+    #     locomotion_cmd=np.zeros(3) # Define which Navigator-Boss the locomotion cotroller will have.
+    # )
+    # # TBD: Future definition of Navigation Policy
+    # top_controller.navigation_policy_ = NavigationPolicy(
+    #         default_angles=np.array(model.keyframe("home").qpos[7:]), #TBD: FIX THOSE DEFAULT ANGELS
+    #         n_substeps=n_substeps,  #TBD: FIX THOSE DEFAULT N_SUBSTEPS
+    #         action_scale=0.5,
+    #     )
+    # # Init time variables
+    # top_controller.t_last_cmd = data.time
+    # top_controller.dt_new_cmd = 0.5 #0.5sec - update vel command per delta t
 
-    # Set the main controll callback from Top Controller
-    mujoco.set_mjcb_control(top_controller.control_callback)
+    # # Set the main controll callback from Top Controller
+    # mujoco.set_mjcb_control(top_controller.control_callback)
 
     return model, data
 
